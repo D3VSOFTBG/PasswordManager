48d6bcafa644cafb51a13d4fe3528dc814db9990
D3VBG <59423787+D3VBG@users.noreply.github.com>
2020-04-26 22:51:57 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/PasswordManager.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PasswordManager.dir/main.c.o"
+  "PasswordManager"
+  "PasswordManager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PasswordManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
